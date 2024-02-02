c6f8d99ef5de17c6cfc0bb2b0766d9d6b4e69760
Ryan Linton <ryanlntn@gmail.com>
2024-02-01 20:36:56 -0700

    update switch

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/views/switchview/ReactSwitch.java
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/views/switchview/ReactSwitch.java
@@ -9,6 +9,7 @@ package com.facebook.react.views.switchview;
 
 import android.content.Context;
 import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
@@ -58,7 +59,7 @@ import androidx.appcompat.widget.SwitchCompat;
   public void setBackgroundColor(long color) {
     setBackground(
         new RippleDrawable(
-            createRippleDrawableColorStateList((int)color), new ColorDrawable((int)color), null));
+            createRippleDrawableColorStateList(Color.toArgb(color)), new ColorDrawable(Color.toArgb(color)), null));
   }
 
   void setColor(Drawable drawable, @Nullable Integer color) {
